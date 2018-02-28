da5ab4ebf652703d7ba1c9048d816b28b38998e0
Ben Godfrey <BenJetson@users.noreply.github.com>
2018-02-28 09:49:53 -0500

    negate

--- classes/Picture.java
+++ classes/Picture.java
@@ -110,6 +110,18 @@ public class Picture extends SimplePicture
       }
     }
   }
+
+  public void negate() {
+    Pixel[][] pixels = this.getPixels2D();
+
+    for (Pixel[] rowArray: pixels) {
+      for (Pixel pixelObj: rowArray) {
+        pixelObj.setRed(255-pixelObj.getRed());
+        pixelObj.setBlue(255-pixelObj.getBlue());
+        pixelObj.setGreen(255-pixelObj.getGreen());
+      }
+    }
+  }
   
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
